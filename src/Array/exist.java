f22681aa6905b465f6b8c7727a7514ac2eefdab2
lc222 <254557889@qq.com>
2017-04-24 22:46:22 +0800

    博客地址：http://blog.csdn.net/liuchonge/article/details/70666474

--- src/Array/exist.java
+++ src/Array/exist.java
@@ -54,6 +54,6 @@ public class exist {
     public static void main(String[] args){
         char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
         String word = "ABCCED";
-        exist(board, word);
+        //exist(board, word);
     }
 }
