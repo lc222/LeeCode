f3c4e163e426c5d08fb69705aef09d18db84f92e
lc222 <254557889@qq.com>
2017-05-02 16:13:00 +0800

    博客地址：http://blog.csdn.net/liuchonge/article/details/71058507

---
+++ src/Array/maxProduct.java
@@ -0,0 +1,22 @@
+package Array;
+
+/**
+ * Created by liuchong on 2017/5/1.
+ */
+public class maxProduct {
+    public int maxProduct(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int max = A[0], min = A[0], result = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
+            min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
+    }
+}
