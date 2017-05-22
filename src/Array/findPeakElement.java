ff9f8cbeb627ffa5d386e7820f3977a8eb4b4cfa
lc222 <254557889@qq.com>
2017-05-22 20:04:28 +0800

    博客地址：http://blog.csdn.net/liuchonge/article/details/72629566

--- src/Array/findPeakElement.java
+++ src/Array/findPeakElement.java
@@ -5,6 +5,26 @@ package Array;
  */
 public class findPeakElement {
     public int findPeakElement(int[] nums) {
+        if(nums.length <=1 || nums[0] > nums[1])
+            return 0;
+        for(int i=1; i<nums.length-1; i++){
+            if(nums[i] > nums[i+1])
+                return i;
+        }
+        return nums.length-1;
+    }
 
+    public int findPeakElement1(int[] nums) {
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] < nums[mid+1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
     }
 }
