2a801d235f22830a9c0bf177dba5476bf647bfa5
lc222 <254557889@qq.com>
2017-06-15 09:53:36 +0800

    博客地址：http://blog.csdn.net/liuchonge/article/details/73274871

--- src/Array/maxProfit3.java
+++ src/Array/maxProfit3.java
@@ -42,4 +42,21 @@ public class maxProfit3 {
         }
         return a;
     }
+
+    public int maxProfit2(int[] prices) {
+        // these four variables represent your profit after executing corresponding transaction
+        // in the beginning, your profit is 0.
+        // when you buy a stock ,the profit will be deducted of the price of stock.
+        int firstBuy = Integer.MIN_VALUE, firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE, secondSell = 0;
+
+        for (int curPrice : prices) {
+            if (firstBuy < -curPrice) firstBuy = -curPrice; // the max profit after you buy first stock
+            if (firstSell < firstBuy + curPrice) firstSell = firstBuy + curPrice; // the max profit after you sell it
+            if (secondBuy < firstSell - curPrice) secondBuy = firstSell - curPrice; // the max profit after you buy the second stock
+            if (secondSell < secondBuy + curPrice) secondSell = secondBuy + curPrice; // the max profit after you sell the second stock
+        }
+
+        return secondSell; // secondSell will be the max profit after passing the prices
+    }
 }
