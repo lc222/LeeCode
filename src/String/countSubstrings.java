092ea21a148366c9c0bff33f68193fdbafdd0dbd
LiuChong <254557889@qq.com>
2017-11-22 14:57:57 +0800

    博客地址：http://blog.csdn.net/liuchonge/article/details/78603748

---
+++ src/String/countSubstrings.java
@@ -0,0 +1,59 @@
+package String;
+
+/**
+
+ 647. Palindromic Substrings
+
+ Given a string, your task is to count how many palindromic substrings in this string.
+
+ The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+ Example 1:
+ Input: "abc"
+ Output: 3
+ Explanation: Three palindromic strings: "a", "b", "c".
+ Example 2:
+ Input: "aaa"
+ Output: 6
+ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ Note:
+ The input string length won't exceed 1000.
+
+ */
+
+
+
+public class countSubstrings {
+
+    int count = 0;
+
+    public int countSubstrings(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
+        }
+
+        return count;
+    }
+
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++; left--; right++;
+        }
+    }
+
+    public int countSubstrings1(String s) {
+        int n = s.length();
+        int res = 0;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+                if(dp[i][j]) ++res;
+            }
+        }
+        return res;
+    }
+}
