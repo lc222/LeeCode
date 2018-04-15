8e954de221182bac0385969640ba5ec70fc06b5b
LiuChong <254557889@qq.com>
2018-04-15 17:08:30 +0800

    合并K个有序链表，使用最大堆最小队的思路

--- src/LinkedList/mergeKLists.java
+++ src/LinkedList/mergeKLists.java
@@ -1,5 +1,9 @@
 package LinkedList;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /**
  * Created by liuchong on 2017/7/16.
  * question:
@@ -46,5 +50,37 @@ public class mergeKLists {
             l2.next=merge(l1,l2.next);
             return l2;
         }
+    }
+
+    public ListNode mergeKLists1(List<ListNode> lists) {
+        if (lists==null||lists.size()==0) return null;
+
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else
+                    return 1;
+            }
+        });
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
     }
 }
